4e7493190753a1aba04f727eaeefa8e71b7ecba9
marcelo.canhoto <marcelo.canhoto@savemais.com.br>
2020-05-06 17:31:28 -0300

    login

--- app/Controllers/Http/SessionController.js
+++ app/Controllers/Http/SessionController.js
@@ -1,12 +1,14 @@
 'use strict'
 
 class SessionController {
-    async create ({ request, auth }) { 
-        const { email, password } = request.all()
-    
-        const token = await auth.attempt(email, password)
-    
-        return token
+    async create ({ request, auth, response }) { 
+        const { email, senha } = request.all()
+        try {
+          const token = await auth.attempt(email, senha)
+          return response.status(201).send({ content: JSON.stringify(token), message: "Login efetuado com successo."})
+        } catch (error) {
+          return response.status(401).send({ content: null, message: 'Login invalido.' })
+        }
     }
 }
 
